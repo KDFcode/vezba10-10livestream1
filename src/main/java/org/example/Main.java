938fc551a8413bcca8a8050d9e41728eee481bf8
KDFcode <aleksandarristic93@gmail.com>
2022-11-09 15:32:48 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+// package con.company; //njemu stoji to,sto meni to pravi problem?
+
+public class  Main {
+    public static void main(String[] args) {
+        System.out.println("first personal GIT project");
+    }
+}
